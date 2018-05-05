ac8dfe091dac5d6ca577306023d32758b8249b2f
Liz Gunter <midwestunicorn@gmail.com>
2018-05-05 21:03:51 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,4 +1,6 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+
+let index = 0;
 
 function init() {
   document.body.addEventListener()
