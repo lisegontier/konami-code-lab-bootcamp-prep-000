d2f6ffad195df562b52636b1285a575db2105dda
Liz Gunter <midwestunicorn@gmail.com>
2018-05-05 21:10:26 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,5 +3,16 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 
 function init() {
+  let i = 0;
   
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+     if (key === code[i]) {
+       i++;
+       
+       if (i === code.length) {
+         alert
+       }
+     }
+  }
 }
